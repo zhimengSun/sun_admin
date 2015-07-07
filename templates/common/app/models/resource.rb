c58e6826673852485adccbe467c9320e34d2fd82
zhimeng.sun <zhimeng.sun@qunar.com>
2015-07-07 11:33:03 +0800

    add parent relations for resources self

--- templates/common/app/models/resource.rb
+++ templates/common/app/models/resource.rb
@@ -1,4 +1,19 @@
 class Resource < ActiveRecord::Base
   has_and_belongs_to_many :roles
   validates :name, :url, presence: true
+
+  scope :supers, -> {where(parent_id: nil)}
+
+  def parent
+    Resource.find_by_id(self.parent_id)
+  end
+
+  def parent_name
+    parent.try(:name)
+  end
+
+  def self.select_values(tag, obj)
+     return supers.collect{|a| [a.name, a.id]} if tag.to_s == "parent_id"
+     super 
+  end
 end
---
+++ templates/common/config/initializers/awesome_form_attributes.rb
@@ -0,0 +1,7 @@
+AwesomeFormAttributes.configure do |config|
+  # config.default_tag = "text_field"
+  # config.text_area_words += []
+  config.select_words += %w(父)
+  # config.boolean_words += []
+  # config.file_words += []
+end

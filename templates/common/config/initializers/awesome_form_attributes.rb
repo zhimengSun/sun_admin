5995ee93d0fac24906436ce9b40f29b3a31f242d
zhimengsun <zhimengsun@gmail.com>
2016-06-24 16:31:05 +0800

    upgrade awesome_form_attributes

--- templates/common/config/initializers/awesome_form_attributes.rb
+++ templates/common/config/initializers/awesome_form_attributes.rb
@@ -4,5 +4,5 @@ AwesomeFormAttributes.configure do |config|
   # config.text_area_words += []
   config.select_words += %w(父)
   # config.boolean_words += []
-  # config.file_words += []
+  # config.file_field_words += []
 end
